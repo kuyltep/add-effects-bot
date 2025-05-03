020c524c13af008ce1d26d41dcbe7e86cacb184e
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-05-03 08:23:42 +0300

    add test logic

--- prisma/migrations/20250503051335_init/migration.sql
+++ prisma/migrations/20250503051335_init/migration.sql
@@ -4,6 +4,12 @@ CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 -- CreateEnum
 CREATE TYPE "GenerationStatus" AS ENUM ('PENDING', 'PROCESSING', 'COMPLETED', 'FAILED');
 
+-- CreateEnum
+CREATE TYPE "Resolution" AS ENUM ('SQUARE', 'VERTICAL', 'HORIZONTAL');
+
+-- CreateEnum
+CREATE TYPE "Language" AS ENUM ('EN', 'RU');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -19,6 +25,9 @@ CREATE TABLE "User" (
     "telegramUsername" TEXT,
     "telegramChatId" TEXT,
     "role" "Role" NOT NULL DEFAULT 'USER',
+    "isBanned" BOOLEAN NOT NULL DEFAULT false,
+    "banReason" TEXT,
+    "bannedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -39,7 +48,7 @@ CREATE TABLE "Payment" (
     "userId" TEXT NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
     "status" TEXT NOT NULL,
-    "transactionId" TEXT,
+    "transactionId" SERIAL NOT NULL,
     "subscriptionType" TEXT,
     "subscriptionDays" INTEGER,
     "generationsAdded" INTEGER,
@@ -63,9 +72,27 @@ CREATE TABLE "Generation" (
     "translatedPrompt" TEXT,
     "status" "GenerationStatus" NOT NULL DEFAULT 'PENDING',
     "error" TEXT,
+    "chatId" TEXT,
+    "messageId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Generation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserSettings" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "useNegativePrompt" BOOLEAN NOT NULL DEFAULT false,
+    "useSeed" BOOLEAN NOT NULL DEFAULT false,
+    "batchSize" INTEGER NOT NULL DEFAULT 3,
+    "resolution" "Resolution" NOT NULL DEFAULT 'HORIZONTAL',
+    "model" TEXT NOT NULL DEFAULT 'rev3',
+    "language" "Language" NOT NULL DEFAULT 'EN',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserSettings_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -83,6 +110,9 @@ CREATE UNIQUE INDEX "Referral_invitedUserId_key" ON "Referral"("invitedUserId");
 -- CreateIndex
 CREATE UNIQUE INDEX "Payment_transactionId_key" ON "Payment"("transactionId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSettings_userId_key" ON "UserSettings"("userId");
+
 -- AddForeignKey
 ALTER TABLE "Referral" ADD CONSTRAINT "Referral_referrerId_fkey" FOREIGN KEY ("referrerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -94,3 +124,6 @@ ALTER TABLE "Payment" ADD CONSTRAINT "Payment_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Generation" ADD CONSTRAINT "Generation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserSettings" ADD CONSTRAINT "UserSettings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
